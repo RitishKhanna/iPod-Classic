25b6398308f24f22136816b05688613afcc62c08
Ritish Khanna <ritishkhanna18@gmail.com>
2021-03-18 15:00:14 +0530

    Updates

---
+++ precache-manifest.e7a2ed183f4e42e0142bfd79ca82e97c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9bd0537337c151303062fd77c5961c",
+    "url": "/iPod-Classic/index.html"
+  },
+  {
+    "revision": "e8df853d75f9765e9d0d",
+    "url": "/iPod-Classic/static/css/main.07582a47.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/iPod-Classic/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/iPod-Classic/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8df853d75f9765e9d0d",
+    "url": "/iPod-Classic/static/js/main.43fc913a.chunk.js"
+  },
+  {
+    "revision": "4deacc06a44225f75abb",
+    "url": "/iPod-Classic/static/js/runtime-main.6904ec8c.js"
+  },
+  {
+    "revision": "092c7d83566618c649286ba2531d1171",
+    "url": "/iPod-Classic/static/media/AudioTapes.092c7d83.png"
+  },
+  {
+    "revision": "a561f22a2401917d39f9dd336a3b7f57",
+    "url": "/iPod-Classic/static/media/Besomorph  Silent Child  IDGAF.a561f22a.png"
+  },
+  {
+    "revision": "e5374a4db2fa8fa48e3051435e17e524",
+    "url": "/iPod-Classic/static/media/Besomorph  Silent Child  IDGAF.e5374a4d.mp3"
+  },
+  {
+    "revision": "9365a8e759f1b3f28bec228c1726b660",
+    "url": "/iPod-Classic/static/media/Billie Eilish  Six Feet Under.9365a8e7.png"
+  },
+  {
+    "revision": "9cd2a7ccf2f56bea302d5fe3bb8aa6f5",
+    "url": "/iPod-Classic/static/media/Billie Eilish  Six Feet Under.9cd2a7cc.mp3"
+  },
+  {
+    "revision": "a4aaa9f22129f50f82fd71023e44cd40",
+    "url": "/iPod-Classic/static/media/BlueCubes.a4aaa9f2.png"
+  },
+  {
+    "revision": "5bb3fcdf00d62055bf72f743161dc3ea",
+    "url": "/iPod-Classic/static/media/BlueJelly.5bb3fcdf.png"
+  },
+  {
+    "revision": "48226638a200f30df574093f6cc4336e",
+    "url": "/iPod-Classic/static/media/Confetti  Ghost.48226638.png"
+  },
+  {
+    "revision": "6f01b1b3314d1c9d7b90588f1080ef8a",
+    "url": "/iPod-Classic/static/media/Confetti  Ghost.6f01b1b3.mp3"
+  },
+  {
+    "revision": "45dcb47f56b6e39e1fa4a26f3811628d",
+    "url": "/iPod-Classic/static/media/DHARIA  Sugar  Brownies.45dcb47f.mp3"
+  },
+  {
+    "revision": "56c55738f9cb8f519d7822de1fef399e",
+    "url": "/iPod-Classic/static/media/DHARIA  Sugar  Brownies.56c55738.png"
+  },
+  {
+    "revision": "1ef224038b615ad7b64100a177f489cc",
+    "url": "/iPod-Classic/static/media/Jony Love Your Voice.1ef22403.png"
+  },
+  {
+    "revision": "a2a2fa838883b86197f656b3fb422a71",
+    "url": "/iPod-Classic/static/media/Jony Love Your Voice.a2a2fa83.mp3"
+  },
+  {
+    "revision": "941e51f3f98607cdb5ac3135f29024f7",
+    "url": "/iPod-Classic/static/media/game.941e51f3.jpg"
+  },
+  {
+    "revision": "994f8bd3084b260278068de214632e59",
+    "url": "/iPod-Classic/static/media/settings.994f8bd3.png"
+  }
+]);
